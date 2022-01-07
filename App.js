e184b8cf681648f03de2b2153ccc0ab1193b7225
Iamsej <iamsamueljones@gmail.com>
2022-01-07 15:31:28 -0500

    initial set up

---
+++ App.js
@@ -0,0 +1,52 @@
+const Blog = require('./models/blog')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ 'logo' } className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
