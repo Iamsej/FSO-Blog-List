126415c4d0d69193c1dff1c9a7846496532ed665
Iamsej <iamsamueljones@gmail.com>
2022-01-08 21:37:31 -0500

    4.7 functional

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,3 @@
-// const _ = require('lodash')
-
 const dummy = (blogs) => {
   return(1)
 }
@@ -13,8 +11,6 @@ const totalLikes = (blogs) => {
         ? likesCount.reduce(reducer)
         : 0
 }
-
-// const favoriteAuthor
 
 const favoriteBlog = (blogs) => {
     const reducer = (first, second) => {
@@ -37,8 +33,52 @@ const favoriteBlog = (blogs) => {
 
 }
 
+const mostBlogs = (blogs) => {
+    const authList = blogs.map(blog => blog.author)
+    const mostFrequent = (array) => {
+        const hashmap = array.reduce( (key, val) => {
+            key[val] = (key[val] || 0 ) + 1
+            return key
+        },{})
+        return Object.keys(hashmap).reduce(
+            (a, b) => hashmap[a] > hashmap[b] ? [a, hashmap[a]] : [b, hashmap[b]], [])
+    }
+    const mostAuth = mostFrequent(authList)
+    
+    if (authList.length === 0) {
+        return {error: 'List is empty'}
+    } else if (authList.length === 1) {
+        return {author: authList[0], blogs: 1}
+    } else {
+        return({author: mostAuth[0], blogs: mostAuth[1]})
+    }
+}
+
+const mostLikes = (blogs) => {
+    const authList = blogs.map(blog => {return({author: blog.author, likes: blog.likes})})
+    const likeSum = (array) => {
+        let midHash = {}
+        for (let variable in array) {
+            midHash[array[variable].author] = (midHash[array[variable].author] || 0) + array[variable].likes
+        }
+        return midHash
+    }
+    const midList = likeSum(authList)
+    console.log(midList)
+    const end = Object.keys(midList).reduce(
+        (a, b) => midList[a] > midList[b] ? a : b, null)
+    
+    if (!end) {
+        return {error: 'List is empty'}
+    } else {
+        return {author: end, likes: midList[end]}
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
