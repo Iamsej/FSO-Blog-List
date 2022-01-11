6e21d12e3e8cb296431f9cfe10ca6f7d34138b36
Iamsej <iamsamueljones@gmail.com>
2022-01-11 15:52:19 -0500

    4.14 functional

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -21,4 +21,32 @@ blogsRouter.post('/', async (request, response) => {
 
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  const result = await Blog.findByIdAndDelete(request.params.id)
+  if (result) {
+    response.statusMessage = 'data successfully deleted'
+    response.status(202).end()
+  } else {
+    response.statusMessage = 'blog not found'
+    response.status(404).end()
+  }
+})
+
+blogsRouter.patch('/:id', async (request, response) => {
+  const newLikes = request.body.likes
+  const result = await Blog
+    .findByIdAndUpdate(
+      request.params.id, 
+      { likes: newLikes }, 
+      { runValidators: true, context: 'query', new:true }
+    )
+  
+  if (!result) {
+    response.statusMessage = 'blog not found'
+    response.status(404).end()
+  } else {
+    response.status(202).json(result)
+  }
+})
+
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,6 +5,7 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const helper = require('./test_helper')
 
+
 beforeEach(async () => {
     await Blog.deleteMany({})
     
@@ -32,6 +33,66 @@ test('blog authors are retrieved correctly', async () => {
     
     const authors = response.body.map(blog => blog.author)
     expect(authors).toContain('Edsger W. Dijkstra')
+})
+
+test('blogs have id instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    const keys = Object.keys(response.body[0])
+    
+    expect(keys).toContain('id')
+})
+
+test('POST saves blogs properly', async () => {
+    const blog = new Blog({
+        title: 'The Angry GM',
+        author: 'Scott Rehm',
+        url: 'https://theangrygm.com/',
+    })
+    await api
+        .post('/api/blogs')
+        .send(blog)
+        .expect(201)
+})
+
+test('if likes is missing, default to 0', async () => {
+    const blog = new Blog({
+        title: 'The Angry GM',
+        author: 'Scott Rehm',
+        url: 'https://theangrygm.com/',
+    })
+    
+    await api
+        .post('/api/blogs')
+        .send(blog)
+        .expect(201)
+
+    const resultBlog = await helper.blogsInDb()
+
+    expect(resultBlog[0].likes).toBe(0)
+})
+
+test('if title or url are missing, 400', async () => {
+    const noURL = new Blog({
+        title: 'The Angry GM',
+        author: 'Scott Rehm',
+        likes: 10
+    })
+
+    const noTitle =new Blog({
+        author: 'Scott Rehm',
+        url: 'https://theangrygm.com/',
+        likes: 10
+    })
+
+    await api
+        .post('/api/blogs')
+        .send(noURL)
+        .expect(400)
+    
+    await api
+        .post('/api/blogs')
+        .send(noTitle)
+        .expect(400)
 })
 
 afterAll(() => {
